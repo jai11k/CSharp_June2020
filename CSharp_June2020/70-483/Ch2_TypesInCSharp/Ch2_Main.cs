1df9c90c3f9a36d18a8afa215661328472726e55
jai11k <jai11simran@gmail.com>
2020-07-26 01:57:20 +0530

    test commit

---
+++ CSharp_June2020/70-483/Ch2_TypesInCSharp/Ch2_Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ch2_TypesInCSharp
+{
+    class Ch2_Main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
