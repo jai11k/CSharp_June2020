5c6ecae8b5e575c28013975106afae4b5f5310d6
jai11k <jai11simran@gmail.com>
2020-07-27 01:49:52 +0530

    chapter 2 doing 27 july

---
+++ CSharp_June2020/70-483/Ch2_TypesInCSharp/TypeConversion/AsKeyword.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Ch2_TypesInCSharp
+{
+    #region jaankari
+    /*
+    as is a keyword used to explicitly convert one type to another.If a type converts
+    successfully, it would return value in that type. If a type doesn’t convert properly,
+    it returns null value.
+    */
+    #endregion
+
+    class A
+    {
+
+    }
+    class B : A
+    {
+
+    }
+
+    public class AsKeyword
+    {
+        static void Main(string[] args)
+        {
+            A a = new B();
+
+            //convert explicitly from type A to B
+            B b = a as B;
+        }
+
+    }
+}
---
+++ CSharp_June2020/70-483/Ch2_TypesInCSharp/UserDefinedTypeConversion/ImplicitUserDefinedConversion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace CSharp_June2020.Ch2_TypesInCSharp.UserDefinedTypeConversion
+{
+    class Byte
+    {
+        #region jaankari
+        /*
+       
+        */
+        #endregion
+
+        public int bits = 8;
+
+        public static implicit operator int(Byte b)
+        {
+            return b.bits;
+        }
+    }
+
+    public class ImplicitUserDefinedConversion
+    {
+        static void Main(string[] args)
+        {
+            Byte b = new Byte();
+            int totalBits = b;
+            Console.WriteLine(totalBits);
+        }
+    }
+}
