d9848092a81d26aba53a8cbbcdfdfcc16a7483fb
jai11k <jai11simran@gmail.com>
2020-07-27 22:32:51 +0530

    chapter 2 about to finish

---
+++ CSharp_June2020/70-483/Ch2_TypesInCSharp/UserDefinedTypeConversion/ExplicitUserDefinedConversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CSharp_June2020.Ch2_TypesInCSharp.UserDefinedTypeConversion
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public static explicit operator string(Person per)
+        {
+            return per.Name;
+        }
+    }
+
+    public class ExplicitUserDefinedConversion
+    {
+        static void Main(string[] args)
+        {
+            Person per = new Person { Age = 22, Name = "Ali" };
+            string name = (string)per;
+            Console.WriteLine(name);
+        }
+    }
+}
