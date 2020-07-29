b71b07c0f64d904cf790fb57b95ad631c5fcc5f7
jai11k <jai11simran@gmail.com>
2020-07-30 00:33:22 +0530

    ch3 doing

--- CSharp_June2020/70-483/Ch3_OOP/DataProtection/Indexers.cs
+++ CSharp_June2020/70-483/Ch3_OOP/DataProtection/Indexers.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-namespace CSharp_June2020.Ch3_OOP.DataProtection
+using System.Collections.Generic;
+
+namespace Ch3_OOP
 {
+
+    #region jaankari
+    /*
+Indexers are used to encapsulate the value of an array. It behaves and works like property.
+It also uses access specifiers, which give better control to read, write, or manipulate an
+array’s value. It creates a sandbox over an array, which protects it from:
+1. saving false data in an array;
+2. using the wrong index value in an array;
+3. changing the reference of an array from the outer world.
+     */
+    #endregion
+
+
+    public class Indexers
+    {
+        static void Main(string[] args)
+        {
+            Temprature temp = new Temprature();
+            float todayTemp = temp[1]; //read
+            temp[1] = -5.0F; //Write
+
+            Temprature temp2 = new Temprature();
+            float todayTemp2 = temp[1]; //read
+            temp[1] = -5.0F; //Write
+        }
+    }
+
+
+
 
     class Temprature
     {
         //declare private array of float type.
         private float[] weekTemp =
             { 47.5F, 40.0F, 52.5F, 45.5F, 48.0F, 38.0F, 35.7F };
+
+     
 
         //use float indexer, to encapsulate weekTemp
         public float this[int index]
@@ -20,14 +53,10 @@ namespace CSharp_June2020.Ch3_OOP.DataProtection
                 weekTemp[index] = value;
             }
         }
+
+      
+
+
     }
-    public class Indexers
-    {
-        static void Main(string[] args)
-        {
-            Temprature temp = new Temprature();
-            float todayTemp = temp[1]; //read
-            temp[1] = -5.0F; //Write
-        }
-    }
+
 }
